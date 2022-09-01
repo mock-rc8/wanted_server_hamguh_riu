e58374a4a5dc1bdc3f68352c8a5f09399901f632
17wook2 <alsdnr9999@g.skku.edu>
2022-09-01 15:44:32 +0900

    #64 - test code 작성 시작

---
+++ src/test/java/com/mockrc8/app/domain/user/mapper/UserMapperTest.java
@@ -0,0 +1,13 @@
+package com.mockrc8.app.domain.user.mapper;
+
+import org.junit.jupiter.api.Test;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@MybatisTest
+class UserMapperTest {
+
+
+}
